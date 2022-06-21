40a927b1ef6daccdd37cfd7ba4f71b027e1342fd
“Hurair17” <[hurairmohammad17@gmail.com]>
2022-06-21 15:23:05 +0500

    Feat: API end point and Model created

---
+++ lib/core/constant/api_end_point.dart
@@ -0,0 +1,6 @@
+class AppUrl {
+  static const String baseUrl =
+      "https://antonxdemo.com/recruiter_help/public/api/";
+
+  static const String signUp = "${baseUrl}register";
+}
--- lib/core/models/auth_model/signup_model.dart
+++ lib/core/models/auth_model/signup_model.dart
@@ -3,6 +3,20 @@ class SignUpModel {
   String? email;
   String? password;
   String? confirmpassword;
+  String? token;
 
-  SignUpModel({this.email, this.password});
+  SignUpModel(
+      {this.email,
+      this.password,
+      this.confirmpassword,
+      this.fullName,
+      this.token});
+
+  factory SignUpModel.fromJson(Map<String, dynamic> json) => SignUpModel(
+        fullName: json["full_name"],
+        email: json["email"],
+        password: json["password"],
+        confirmpassword: json["confirm_password"],
+        token: json["token"],
+      );
 }
